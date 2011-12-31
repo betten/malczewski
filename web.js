bf8f45d91288767b2d577f6e5368e7c40d309861
betten <betten@bc.edu>
2011-12-31 07:15:19 -0500

    deleting/inserting self portrait images

--- web.js
+++ web.js
@@ -11,25 +11,47 @@ mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db) {
   });
 
   db.collection("selfportraits", function(error, collection) {
-    // load new self portraits into collection, new defined by file name
-    // fs.readdir('./public/selfportraits', function(error, files) {
-    //   if(error) throw error;
-    //   console.dir(files);
-    //   collection.find({ 'filename': { '$nin': files } }, function(err, cursor) {
-    //     cursor.each(function(error, item) {
-    //       console.dir(item);
-    //     });
-    //   });
-    // });
+    fs.readdir('./public/selfportraits', function(error, files) {
+      if(error) throw error;
+      // remove self portraits from collection no longer in selfportraits dir
+      collection.remove({ 'filename': { '$nin': files } });
+      // load new self portraits into collection, new defined by file name
+      collection.find(function(error, cursor) {
+        cursor.toArray(function(error, docs) {
+          docs.forEach(function(doc) {
+            var i = files.indexOf(doc.filename);
+            if(i != -1) files.splice(i, 1);
+          });
+          console.log('new files:');
+          console.dir(files);
+          for(file in files) {
+            collection.insert({ 'filename': file });
+          }
+        });
+      });
+    });
   });
 
 });
 
 app.get('/', function(request, response) {
-  fs.readdir('./public/selfportraits', function(error, files) {
-    if(error) throw error;
-    response.send(files.join("\n"));
+  
+  mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db) {
+
+    db.addListener("error", function(error) {
+      console.log("Error connecting to MongoLab");
+    });
+
+    collection.find(function(error, cursor) {
+      var output = "";
+      cursor.each(function(error, doc) {
+        if(doc != null) output += "<div>id: " + doc._id + ", filename: " + doc.filename + "</div>";
+      });
+      response.send(output);
+    });
+
   });
+
 });
 
 var port = process.env.PORT || 3000;
