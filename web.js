bae727b8d547debabc39f91f83a28067b9eb57f7
betten <betten@bc.edu>
2011-12-31 05:39:17 -0500

    added mongo

--- web.js
+++ web.js
@@ -1,6 +1,15 @@
-var express = require('express');
+var express = require('express'),
+    mongo = require('mongodb');
 
 var app = express.createServer(express.logger());
+
+mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db) {
+
+  db.addListener("error", function(error) {
+    console.log("Error connecting to MongoLab");
+  });
+
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
